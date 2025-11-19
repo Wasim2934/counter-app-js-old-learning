17f454234da3e3b1fea6e919ff842590c104639c
Wasim2934 <mdwasimlesnar0019@gmail.com>
2025-11-20 01:45:30 +0530

    Initial commit: Counter app

---
+++ script.js
@@ -0,0 +1,26 @@
+let count = 0;
+const counterDisplay = document.getElementById('counter');
+const incrementBtn = document.getElementById('increment');
+const decrementBtn = document.getElementById('decrement');
+const resetBtn = document.getElementById('reset');
+
+count = 0;
+
+incrementBtn.addEventListener('click', () => {
+    count++;
+    counterDisplay.textContent = count; 
+});
+
+decrementBtn.addEventListener('click', () => {
+    if (count > 0) {
+        count--;
+       counterDisplay.textContent = count;
+    }
+});
+
+resetBtn.addEventListener('click', () => {
+    count = 0;
+    counterDisplay.textContent = count;
+});
+
+
